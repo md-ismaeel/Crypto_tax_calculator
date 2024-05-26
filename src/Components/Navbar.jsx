ccceaa1e5b2900bfead604d41fffa638edee1fb6
Md Ismail <mdismaeelkhan345@gmail.com>
2024-05-26 17:19:10 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,10 +16,10 @@ export const Navbar = () => {
                 </div>
                 <div className='flex justify-center items-center gap-6 text-lg'>
                     <ul className='navUl flex justify-center items-center gap-7'>
-                        <li>feature</li>
-                        <li>exchanges</li>
-                        <li>how it Works?</li>
-                        <li>bog</li>
+                        <li>Feature</li>
+                        <li>Exchanges</li>
+                        <li>How it Works?</li>
+                        <li>Bog</li>
                         <li>About us</li>
                     </ul>
                     <button className='btn py-[3px] px-4 text-blue-500 rounded-md'>sign in</button>
@@ -39,10 +39,10 @@ export const Navbar = () => {
                 {show && (
                     <div className='absolute z-10 top-12 right-0 justify-center items-start gap-6 text-lg bg-white px-10 py-4 border-2'>
                         <ul className='navUl  flex flex-col justify-center items-start gap-7'>
-                            <li>feature</li>
-                            <li>exchanges</li>
-                            <li>how it Works?</li>
-                            <li>bog</li>
+                            <li>Feature</li>
+                            <li>Exchanges</li>
+                            <li>How it Works?</li>
+                            <li>Bog</li>
                             <li>About us</li>
                         </ul>
                         <button className='btn py-[3px] px-4 mt-2 text-blue-500 rounded-md'>sign in</button>
